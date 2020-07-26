1252b3bbf230571a7bd61cc8fc839576d569d1f1
andreeacirciu <andreeacirciu@gmail.com>
2020-07-26 17:02:14 +0300

    formatted task deadline and completed checkbox

--- js/tasks.js
+++ js/tasks.js
@@ -43,11 +43,17 @@ window.ToDoList = {
     },
 
     getTaskRowHtml: function (task) {
+//spread syntax
+        let formattedDeadline = new Date(...task.deadline).toLocaleDateString('ro');
+
+        //ternary operator
+        let checkedAttribute = task.done ? 'checked' : '';
+
         return ` <tr>
         <td> ${task.description}</td>
-        <td>${task.deadline}</td>
+        <td>${formattedDeadline}</td>
         <td>
-            <input type="checkbox" class="mark-done" data-id=${task.id}>
+            <input type="checkbox" class="mark-done" data-id=${task.id} ${checkedAttribute}>
         </td>
         <td>
             <a href="#" class="remove-task" data-id = ${task.id}> <i class="fa fa-trash"></i> </a>
